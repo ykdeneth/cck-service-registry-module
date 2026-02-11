21891585c4c95a545b77786bd20ef0a136035f84
ykdeneth <denethyak2002@gmail.com>
2026-02-11 08:51:51 +0530

    service-registry

---
+++ src/test/java/com/cck/service_registory/ServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cck.service_registory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
